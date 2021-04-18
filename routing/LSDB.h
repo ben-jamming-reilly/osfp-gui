a0d9dbab0fd06a38147c5ae0a363431913786050
Nicholas Ryan Smith <nsmith10@zagmail.gonzaga.edu>
2021-04-18 00:21:23 -0700

    Finalize LSDB implementation using std::map and begin basic testing

--- routing/LSDB.h
+++ routing/LSDB.h
@@ -9,27 +9,78 @@
 class LSDB
 {
 public:
-    LSDB();
-    ~LSDB();
-
     // Adds the new LSA to the database
     // Inputs: Link State Advertisement (LSA)
     // Outputs: None
-    void add_router_lsa(LSA lsa);
+    void add_router_lsa(RouterLSA lsa);
 
     // Checks if this LSA already exists in the database
     // Inputs: Link State Advertisement (LSA)
     // Outputs: True if it exists, false if not
-    bool router_lsa_exists(LSA lsa);
+    bool router_lsa_exists(RouterLSA lsa);
 
     // Returns all the Link State Advertisements in the database
     // Inputs: None
-    // Outputs: List of Link State Advertisements (LSAs).
-    vector<RouterLSA> advertise_database();
+    // Outputs: Sorted vector list of Link State Advertisements (LSAs).
+    std::vector<RouterLSA> advertise_database();
+
+    // Returns the number of Link State Advertisements in the database
+    // Inputs: None
+    // Outputs: Size of database
+    size_t size();
 
 private:
-    map< pair<int, int>, LSA > router_lsdb;
-    int num_router_lsa;
+    std::map<Link, RouterLSA> router_lsdb;
 };
+
+// checks if newer link state advertisement
+// already exists for the given link
+void LSDB::add_router_lsa(RouterLSA lsa)
+{
+    if (this->router_lsdb.count(lsa.LINK) == 0)
+    {
+        // LSDB does not contain LSA, add to database
+        router_lsdb.insert(std::pair<Link, RouterLSA>(lsa.LINK, lsa));
+    } else if (this->router_lsdb.at(lsa.LINK).SEQ_NUM < lsa.SEQ_NUM)
+    {
+        // LSDB has an old LSA for this link, update database
+        router_lsdb.at(lsa.LINK) = lsa;
+    }
+}
+
+bool LSDB::router_lsa_exists(RouterLSA lsa)
+{
+    bool retVal = false;
+
+    if (this->router_lsdb.count(lsa.LINK) != 0 &&
+        this->router_lsdb.at(lsa.LINK).SEQ_NUM == lsa.SEQ_NUM)
+    {
+        // LSA exists in the database
+        retVal = true;
+    }
+
+    return retVal;
+}
+
+// sort LSAs by link into a vector and return
+std::vector<RouterLSA> LSDB::advertise_database()
+{
+    std::vector<RouterLSA> retVal(this->router_lsdb.size());
+
+    int i = 0;
+    for (std::map<Link, RouterLSA>::iterator lsa = this->router_lsdb.begin();
+         lsa != this->router_lsdb.end(); ++lsa)
+    {
+        retVal.at(i) = lsa->second;
+        ++i;
+    }
+
+    return retVal;
+}
+
+size_t LSDB::size()
+{
+    return this->router_lsdb.size();
+}
 
 #endif
--- routing/ospf_test.cpp
+++ routing/ospf_test.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include "Router.h"
+//#include "Router.h"
+#include "LSDB.h"
+#include "LSA.h"
 
 TEST(InitTest, InitTest)
 {
 	// Used to check if GTest is configured properly
 	EXPECT_EQ(1, 1);
+}
+
+TEST(LSDBTest, AddRouterLSATest)
+{
+	// define some nodes to make debugging easier to read
+	enum node
+	{
+		u = 0,
+		v = 1,
+		w = 2,
+		x = 3
+	};
+
+	RouterLSA lsa = { Link(u, v), INIT_SEQ_NUM, 1};
+	LSDB lsdb;
+	ASSERT_EQ(lsdb.size(), 0);
+	
+	// show that duplicate LSAs won't be added to database
+	lsdb.add_router_lsa(lsa);
+	ASSERT_EQ(lsdb.size(), 1);
+	lsdb.add_router_lsa(lsa);
+	ASSERT_EQ(lsdb.size(), 1);
+
+	// show that the same sequence number can be used for different links
+	lsa = { Link(v, w), INIT_SEQ_NUM, 1};
+	lsdb.add_router_lsa(lsa);
+	ASSERT_EQ(lsdb.size(), 2);
+
+	// show that an LSAs with higher sequence number are prioritized
+	lsa = { Link(v, w), INIT_SEQ_NUM+1, 1};
+	lsdb.add_router_lsa(lsa);
+	ASSERT_EQ(lsdb.size(), 2);
 }
 
 int main(int argc, char** argv)
